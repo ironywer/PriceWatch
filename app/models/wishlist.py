43b5cab13713422e9f5aa671c83f26127bd81abf
ironywer <leva.vtorov@yandex.ru>
2025-10-27 00:12:29 +0300

    [DV]: added db

---
+++ app/models/wishlist.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class Wishlist(Base):
+    __tablename__ = "wishlists"
+
+    id = Column(Integer, primary_key=True, index=True)
+    steam_app_id = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="wishlists")

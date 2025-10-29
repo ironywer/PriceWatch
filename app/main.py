2fc3d22e0df4f5c0f7f7c2130cc52845369af4ed
Jerend <rechkovdv@gmail.com>
2025-10-29 16:20:36 +0300

    Delete all __pycache__ files

--- app/main.py
+++ app/main.py
@@ -5,12 +5,7 @@ from app.routers.search import router as search_router
 
 app = FastAPI(title="PriceWatch — MVP")
 
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 app.include_router(main_router)
 app.include_router(search_router)
-
-
-# app.include_router(api_products_router, prefix="/api", tags=["products"])
